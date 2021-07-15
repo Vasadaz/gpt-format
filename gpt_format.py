60c68479035b57ec7216592054be41aa0ae56126
Sysoev <vasadaz@ya.ru>
2021-07-15 12:17:36 +0300

    Beginning write code and learn module "pexpect"

--- gpt_format.py
+++ gpt_format.py
@@ -5,15 +5,21 @@
 2) Добавить звуковое соправождение с помощью speaker, для слепого форматирования
 """
 
-import subprocess
+#import subprocess
+import pexpect
 
+"""
 try:
     # Передаём в процесс run команду "fdisk -l" ввиде списка ["fdisk", "-l"]
     # capture_output=True При Истине - ответ команды не будет выведен в консоль,
     #   но сохраниться в объекте subprocess.CompletedProcess
     # text=True При Истине - текст stdout сохраняет строко вид, иначе байтовая последовательность.
     # check=True При Истине - поднимается ошибка при её возникновении.
-    cmd_fdisk = subprocess.run(["fdisk", "-l"], capture_output=True, text=True, check=True)
+    sudo_pass = subprocess.Popen(["echo", '"1234"'], stdout=subprocess.PIPE)
+    cmd_sudo_s = subprocess.Popen(["sudo", "-s"], stdin=sudo_pass)
+    cmd_fdisk_l = subprocess.run(["fdisk", "-l"], capture_output=True, text=True, check=True)
+
+
 
 except subprocess.CalledProcessError as e:
     # Поднимаем ошибку с её описанием взятого из объекта subprocess.CalledProcessError
@@ -23,11 +29,11 @@ except subprocess.CalledProcessError as e:
 file_1 = open("road_gpt")
 cmd_fdisk.stdout = file_1.read()
 file_1.close()
+"""
 
-# Ищем последний подключенный диски. a и b диски не трогать - это raid1
-cmd_fdisk_list = [el[:-1] for el in cmd_fdisk.stdout.split() if "/dev/sd" in el and
-                  not "/dev/sda" in el and not "/dev/sdb" in el][0]
+# Ищем последний подключенный диск. a и b диски не трогать - это raid1.
+# cmd_fdisk_list = [el[:-1] for el in cmd_fdisk_l.stdout.split() if "/dev/sd" in el and
+#                  not "/dev/sda" in el and not "/dev/sdb" in el][0]
 
-print(cmd_fdisk_list)
-
+#print(f"Найден диск {cmd_fdisk_list}")
 
