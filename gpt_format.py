7cb0ae2e44c6863de30bd398856e65711535f93e
Sysoev <vasadaz@ya.ru>
2021-07-16 15:10:37 +0300

    Fix and new logs for "fdisk -l" command

--- gpt_format.py
+++ gpt_format.py
@@ -15,36 +15,55 @@ cmd_fdisk_l.expect(pexpect.EOF)
 
 # Преобразование атрибута .before в кодировку utf-8
 #cmd_fdisk_l_stdout = cmd_fdisk_l.before.decode("utf-8")
-#print(cmd_fdisk_l_stdout)
+#print(cmd_fdisk_l_stdout.split("\n"))
 
 #'''
 # Имитация команда fdisk -l
 with open("road_fdisk_l", "r") as road_fdisk_l:
-    cmd_fdisk_l_stdout = road_fdisk_l.readlines()
+    cmd_fdisk_l_stdout = road_fdisk_l.read()
     road_fdisk_l.close()
 
 #print(cmd_fdisk_l_stdout)
 #'''
 
 # Ищем последний подключенный диск. a и b диски не трогать - это raid1.
-activ_disk_list = [el[:-1] for el in str(cmd_fdisk_l_stdout).split() if "/dev/sd" in el and
+activ_disk_list = [el[:-1] for el in cmd_fdisk_l_stdout.split() if "/dev/sd" in el and
                    not "/dev/sda" in el and not "/dev/sdb" in el]
 
 
+
 # Логирование
+if len(activ_disk_list) == 0:
+    print("\n***************\nDisk not found\n***************\n")
+    exit()
 
 for i_disk in range(len(activ_disk_list)):
-    print("*******************************************************")
-    print(f"Disk found INDEX {i_disk}")
-    print("    |     ")
-    print("    V     ")
-    print(activ_disk_list[i_disk], "it info:")
+    print("\n*******************************************************")
+    print("Disk found {} INDEX-{}\n".format(activ_disk_list[i_disk],i_disk))
 
-    for i_search_info_disk in range(len(cmd_fdisk_l_stdout)):
-        if activ_disk_list[i_disk] in cmd_fdisk_l_stdout[i_search_info_disk]:
+    for i_search_info_disk in range(len(cmd_fdisk_l_stdout.split("\n"))):
+        if activ_disk_list[i_disk] in cmd_fdisk_l_stdout.split("\n")[i_search_info_disk]:
             for i_info_disk in range(6):  # Выводим инфу о найденном диске
-                print(cmd_fdisk_l_stdout[i_search_info_disk + i_info_disk].strip())
-    print()
+                try:
+                    print(cmd_fdisk_l_stdout.split("\n")[i_search_info_disk + i_info_disk].strip())
+                except IndexError:
+                    pass
+    print("*******************************************************")
 
-input("How index disk format in GPT? (0)")
-i_gpt_disk = activ_disk_list[0]
+# Выьераем диск из найденого для дальнейшего форматирования
+i_gpt_disk = input("\nHow index disk format in GPT? (0): ")
+if i_gpt_disk == "":
+    gpt_disk = activ_disk_list[0]
+else:
+    gpt_disk = activ_disk_list[int(i_gpt_disk.strip())]
+
+print("*************************************")
+print("Disk {} will be format in GTP".format(gpt_disk))
+print("*************************************")
+
+# Подтверждение
+go_format_gpt = input("\nGo format in GPT? (Yes/no): ")
+if go_format_gpt == "no":
+    exit()
+print("GPT___" * 8)
+
